8e235bddc01ac43b78998dbd410a48f10349209e
Giganotosaurus <75021575+joshrkhoo@users.noreply.github.com>
2023-10-10 21:34:15 +1100

    Completed Cute Numbers

--- cute_numbers.py
+++ cute_numbers.py
@@ -7,3 +7,20 @@ for i in range(a):
 
 print(a, input_nums)
 
+# Traverse the input numbers in reverse order 
+# starting from the last input
+    # if not 0 
+        # not a cute numbers so output should be 0
+    # if 0 
+        # increment count
+        # go to i-1th index and check if it is a 0 
+    #print count
+
+
+count = 0
+for i in range(len(input_nums)-1, -1, -1):
+    if input_nums[i] != 0:
+        break
+    count +=1 
+
+print(count)
