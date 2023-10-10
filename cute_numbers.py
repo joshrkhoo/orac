a2d87d35ef5f7756a15f6b96459e32ef94265262
Giganotosaurus <75021575+joshrkhoo@users.noreply.github.com>
2023-10-10 22:38:23 +1100

    completed cute numbers

--- cute_numbers.py
+++ cute_numbers.py
@@ -1,12 +1,19 @@
 # a, b = map(int, input().split())
-a = int(input())
-
+# a = int(input())
+ 
+# input_nums = []
+# for i in range(a):
+#     input_nums.append(int(input()))
+ 
+inputFile = open("cutein.txt", "r")
+a = None
 input_nums = []
-for i in range(a):
-    input_nums.append(int(input()))
-
-print(a, input_nums)
-
+for i, line in enumerate(inputFile.readlines()):
+    if i == 0:
+        a = int(line.strip())
+        continue
+    input_nums.append(int(line.strip()))
+ 
 # Traverse the input numbers in reverse order 
 # starting from the last input
     # if not 0 
@@ -15,12 +22,18 @@ print(a, input_nums)
         # increment count
         # go to i-1th index and check if it is a 0 
     #print count
-
-
+ 
+ 
 count = 0
 for i in range(len(input_nums)-1, -1, -1):
     if input_nums[i] != 0:
         break
     count +=1 
+ 
+# Write the output
+outputFile = open("cuteout.txt", "w")
+outputFile.write(str(count) + "\n")
+ 
+# Clean up!
+inputFile.close()
 
-print(count)
